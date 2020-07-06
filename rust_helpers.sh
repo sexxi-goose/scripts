e4b2046a5c5e7ed4717d79a6ae28e968a90a9d70
Aman Arora <me@aman-arora.com>
2020-07-06 00:04:34 -0400

    Add Rust helpers

---
+++ rust_helpers.sh
@@ -0,0 +1,56 @@
+#!/bin/bash
+
+SG_RUST_PROJECT_REPO=$HOME/rust
+SG_RUST_BUILD_LOG_DIR=$HOME/www/rust-builds
+export SG_RUST_BUILD_LOG=$HOME/www/rust-log-latest
+
+runRustStuff()
+{
+    set -e
+
+    mkdir -p $SG_RUST_BUILD_LOG_DIR
+    pushd $SG_RUST_PROJECT_REPO
+
+    local FILE_SUFFIX=$(uuidgen)
+
+    if [[ $1 == "test-commit" ]]; then
+        FILE_SUFFIX="commit_"$FILE_SUFFIX_$(git rev-parse HEAD)
+        shift
+    fi
+
+    local build_file="$SG_RUST_BUILD_LOG_DIR/log_$FILE_SUFFIX"
+    touch $build_file
+    ln -sf $build_file $SG_RUST_BUILD_LOG
+
+    ./x.py fmt
+    ./x.py $@ |& tee $build_file
+    popd
+}
+
+buildRust()
+{
+    runRustStuff build -j50 -i --stage 1 $@
+}
+
+testRust()
+{
+    runRustStuff test -i --stage 1 $@
+}
+
+
+openBuildLog()
+{
+    vim $SG_RUST_BUILD_LOG
+}
+
+testRustCommit()
+{
+    runRustStuff test-commit test -i --stage 1 $@
+}
+
+screenTestCommit()
+{
+    screen -dm zsh -c "source $HOME/rust_stuff.sh; testRustCommit"
+}
+
+
